a80551aec7a785684ecd3fea5046b2d8308cdf05
azashdry <aminuibrahimdorayi002@gmail.com>
2026-06-24 23:32:20 +0100

    Add files via upload
    Kungiyar dake kulada farashin Yan kasuwa Na universities don Gani cewa baza tsawwalawa dalibai farashiba

---
+++ script.js
@@ -0,0 +1,62 @@
+const data = {
+
+ "gas": [
+ // Babu data tukuna
+ ],
+
+ "phone": [
+ // Babu data tukuna
+ ],
+
+ "food": [
+ // Babu data tukuna
+ ],
+
+ "transport": [
+ // Babu data tukuna
+ ],
+
+ "drinks": [
+ // Babu data tukuna
+ ],
+
+ "cosmetic": [
+ // Babu data tukuna
+ ],
+
+ "clothes": [
+ {
+ item: "T-shirt",
+ price: "₦2000",
+ location: "Kasuwar kofar fada",
+ shop: "Aminu shop",
+ phone: "09120982352"
+ },
+
+ {
+ item: "Jeans",
+ price: "₦1500",
+ location: "Anguwan fulani",
+ shop: "Shagon adhama",
+ phone: "091209772728"
+ },
+
+ {
+ item: "Abaya",
+ price: "₦15000",
+ location: "Opposite gidan wuta",
+ shop: "Ma'am Mai sauki",
+ phone: "091209282882"
+ },
+
+ {
+ item: "Hijab",
+ price: "₦3000",
+ location: "Kofar commercial",
+ shop: "Nano",
+ phone: "09019287373791"
+ },
+
+ ],
+
+};
